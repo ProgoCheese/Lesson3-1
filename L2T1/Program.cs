3d7234cbb351c9f1d3356917900bd3e8e2d87393
Artemii100500 <Artemii100500@gmail.com>
2022-12-11 22:22:23 +0700

    Задание для 2 урока

---
+++ L2T1/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Задача Вычисление выраж
+
+        int a = 2 + 1;
+        int b = 2 - 1;
+        int c = 2 * 1;
+        float d = (float)2 / 2;
+        int e = 2 % 2;
+
+        Console.WriteLine("{0} {1} {2} {3} {4}",a,b,c,d,e);
+    }
+}
