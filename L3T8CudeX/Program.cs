2c5aa6e66b29b2713bec268fcc1cb8b14b6ad624
Artemii100500 <Artemii100500@gmail.com>
2022-12-12 21:14:50 +0700

    Задание квадратного уравнения изменения

--- L3T8CudeX/Program.cs
+++ L3T8CudeX/Program.cs
@@ -1,31 +1,33 @@
-﻿
-//Квадратное уравнение
+﻿//Квадратное уравнение
 
 class Program
 {
      static void Main()
     {
-        double a = Convert.ToInt32(Console.ReadLine());
-        double b = Convert.ToInt32(Console.ReadLine());
-        double c = Convert.ToInt32(Console.ReadLine());
-        double x = 1;
+        Console.WriteLine("Программа находит корни квадратного уравнения типа");
+        Console.Write("Введите коэффициент a:");
+        double KofA = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите коэффициент b:");
+        double KofB = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите коэффициент c:");
+        double KofC = Convert.ToInt32(Console.ReadLine());
+        double x;
+        double d = KofB * KofB - 4 * KofA * KofC;
 
-        double d = b * b - 4 * a * c;
-
-        if (a == 0 && b == 0 && c == 0)
+        if (KofA == 0 && KofB == 0 && KofC == 0)
         {
             Console.WriteLine("Нет корня");
         }
         else if (d > 0)
         {
-            x = -b + Math.Sqrt(d) / 2 * a;
-            double x2 = -b - Math.Sqrt(d) / 2 * a;
-            Console.WriteLine("Первый корень равен " + x + ". Второй корень равен " + x2);
+            x = -KofB + Math.Sqrt(d) / 2 * KofA;
+            double x2 = -KofB - Math.Sqrt(d) / 2 * KofA;
+            Console.WriteLine("Первый корень равен {0} Второй корень равен {1}",x,x2);
         }
         else if (d == 0)
         {
-            x = -(b / 2 * a);
-            Console.WriteLine("Корень равен " + x);
+            x = -(KofB / 2 * KofA);
+            Console.WriteLine("Корень равен {0}",x);
         }
         else if (d < 0)
         {
