8cd6e3a52ae72313b32e80db36b881a23645cefe
Artemii100500 <Artemii100500@gmail.com>
2022-12-12 21:53:10 +0700

    Изменил високосный год

--- L3T5Year/Program.cs
+++ L3T5Year/Program.cs
@@ -1,11 +1,22 @@
-﻿//Задача Високосный год
-
-int year = Convert.ToInt32(Console.ReadLine());
-if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
+﻿///Задача Следующая дата
+using System;
+class Sample
 {
-    Console.WriteLine("Год високосный");
-}
-else
-{
-    Console.WriteLine("Год невисокосный");
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Программа рассчитывает високосный ли год.");
+            Console.WriteLine("Введите год:");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0)
+            {
+                Console.WriteLine("Год високосный");
+            }
+            else
+            {
+                Console.WriteLine("Год невисокосный");
+            }
+        }
+    }
 }
