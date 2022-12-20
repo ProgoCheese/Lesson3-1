5113e23b1627d2de6ae9f74e5393b6610b6946ca
Artemii100500 <Artemii100500@gmail.com>
2022-12-20 22:30:40 +0700

    1

---
+++ L4T1/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите команду{print,sum}");
+        string a = Console.ReadLine();
+        switch (a)
+        {
+            case "print":
+                Console.WriteLine("Напечатать строчку: ");
+                a = Console.ReadLine();
+                Console.WriteLine(a);
+                break;
+            case "sum":
+                Console.WriteLine("Введите число 1:");
+                int c = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите число 2:");
+                int b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(c + b);
+                break;
+            default:
+                Console.WriteLine("Неверная команда");
+                break;
+        }
+    }
+}
--- Lesson3,1/Program.cs
+++ Lesson3,1/Program.cs
@@ -1,8 +1,20 @@
-﻿class Program
+﻿/// <summary>
+/// Задача Вычисление выраж
+/// </summary>
+class Program
 {
     static void Main()
     {
-        // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World!");
+        double a = Convert.ToDouble(Console.ReadLine());
+        double b = Convert.ToDouble(Console.ReadLine());
+        double e = 1.0e-10;
+        if(Math.Abs(a - b) <= e)
+        {
+            Console.WriteLine("Числа равны" );
+        }
+        else
+        {
+            Console.WriteLine("Числа неравны");
+        }
     }
 }
