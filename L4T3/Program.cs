c3c0dd0ef69e9f0a72c4d16fde7fae602c2f23d9
Artemii100500 <Artemii100500@gmail.com>
2022-12-23 20:12:30 +0700

    l

---
+++ L4T3/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число от 1 до 4: ");
+        int comand = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите число 1:");
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите число 2:");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        switch (comand)
+        {
+            case 1:
+                Console.WriteLine(firstNumber + secondNumber);
+                break;
+            case 2:
+                Console.WriteLine(firstNumber - secondNumber);
+                break;
+            case 3:
+                Console.WriteLine(firstNumber * secondNumber);
+                break;
+            case 4:
+                Console.WriteLine(firstNumber / secondNumber);
+                break;
+            default:
+                Console.WriteLine("Неверная команда");
+                break;
+        }
+    }
+}
--- Lesson3,1/Program.cs
+++ Lesson3,1/Program.cs
@@ -5,10 +5,15 @@ class Program
 {
     static void Main()
     {
-        double a = Convert.ToDouble(Console.ReadLine());
-        double b = Convert.ToDouble(Console.ReadLine());
-        double e = 1.0e-10;
-        if(Math.Abs(a - b) <= e)
+        Console.Write("Введите 1 число: ");
+        double firstNumber = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите 2 число: ");
+        double secondNumber = Convert.ToDouble(Console.ReadLine());
+
+        const double epsilon = 1.0e-10;
+
+        if(Math.Abs(firstNumber - secondNumber) <= epsilon)
         {
             Console.WriteLine("Числа равны" );
         }
