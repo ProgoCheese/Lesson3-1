888139d71b3c9c1bb960c47e190448830d2d1938
Artemii100500 <Artemii100500@gmail.com>
2022-12-25 13:30:38 +0700

    1

---
+++ L4T4/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+        int i = 3;
+        int endNumber = 21;
+        int numberCount = 0;
+
+        while (i <= endNumber)
+        {
+            if (i % 2 == 0)
+            {
+                sum += i;
+                numberCount++;
+            }
+
+            ++i;
+        }
+
+        Console.WriteLine(sum + " " + numberCount);
+    }
+}
---
+++ L4T5/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+        int i = 1;
+        int endNumber = 2;
+        int numberCount = 0;
+
+        while (i <= endNumber)
+        {
+            sum += i;
+            numberCount++;
+
+            ++i;
+        }
+
+        float midNummber = (float)sum / numberCount;
+
+        Console.Write(midNummber + " ");
+
+        sum = 0;
+        i = 3;
+        numberCount = 0;
+
+        while (i <= endNumber)
+        {
+            if (i % 2 == 0)
+            {
+                sum += i;
+                numberCount++;
+            }
+
+            ++i;
+        }
+
+        midNummber = (float)sum / numberCount;
+
+        Console.Write(midNummber);
+    }
+}
