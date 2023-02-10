eff5c9104018c71cb61dc986b727c80dbc1a471d
Artemii100500 <Artemii100500@gmail.com>
2023-02-10 23:27:19 +0700

    123

---
+++ L4T7Whil-do/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите начальное число: ");
+        int beginingNumber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите начальное число: ");
+        int endingNumber = Convert.ToInt32(Console.ReadLine());
+
+        int sum = 0;
+        int countNumber = 0;
+        int i = beginingNumber;
+
+        do
+        {
+            sum += i;
+            countNumber++;
+            i++;
+        }
+        while (i <= endingNumber);
+        
+        double midNumber = (double)sum / countNumber;
+
+        Console.WriteLine("Средние арифмитическое " + midNumber);
+
+        int countNumberEven = 0;
+        i = beginingNumber;
+        int sumEven = 0;
+
+        do
+        {
+            if (i % 2 == 0)
+            {
+                sumEven += i;
+                countNumberEven++;
+            }
+            i++;
+        }
+        while (i <= endingNumber);
+        
+
+        double midNumberEver = (double)sumEven / countNumberEven;
+
+        Console.WriteLine("Средние арифмитическое четных " + midNumberEver);
+    }
+}
---
+++ L4T8Number10InLine/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите начальное число: ");
+        int beginingNumber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите конечное число: ");
+        int endingNumber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите количество чисел в строке: ");
+        int quantityNumber = Convert.ToInt32(Console.ReadLine());
+
+        int i = beginingNumber;
+        int j = 0;
+
+        while (i <= endingNumber)
+        {
+            j = 0;
+            while (j < quantityNumber)
+            {
+                j++;
+                Console.Write(i.ToString().PadLeft(5));
+                i++;
+            }
+            Console.WriteLine();
+        }
+    }
+}
