8f3084353f0b2ca64add052f5f76a40e906417f8
Artemii100500 <Artemii100500@gmail.com>
2023-08-10 14:06:06 +0700

    s

---
+++ L5T7/Program.cs
@@ -0,0 +1,37 @@
+﻿/// <summary>
+/// Алгоритм евклида
+/// </summary>
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите первое число");
+        int firstNumber = Convert.ToInt32(Console.ReadLine()); 
+
+        Console.WriteLine("Введите второе число");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+        bool isEnd = false;
+
+        int greatestСommonВivisor = 1;
+
+        while (isEnd != true)
+        {
+            int remainder = firstNumber % secondNumber;
+
+            if (remainder == 0)
+            {
+                isEnd = true;
+                greatestСommonВivisor = secondNumber;
+            }
+            else
+            {
+                firstNumber = secondNumber;
+                secondNumber = remainder;
+                continue;
+            }
+        }
+
+        Console.WriteLine("НОД = {0}",greatestСommonВivisor);
+    }
+}
