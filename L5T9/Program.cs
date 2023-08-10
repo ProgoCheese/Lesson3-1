607c458995e12d8f5e237d69ba9b4d10e9d1abd7
Artemii100500 <Artemii100500@gmail.com>
2023-08-10 18:43:26 +0700

    s

---
+++ L5T9/Program.cs
@@ -0,0 +1,32 @@
+﻿/// <summary>
+/// Вклад
+/// </summary>
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите сумму вклада:");
+        double depositAmount = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите ставку годовых (%):");
+        double interestRate = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите число месяцев:");
+        int months = Convert.ToInt32(Console.ReadLine());
+
+        double monthInterestRate = interestRate / 100 / 12;
+
+        double totalAmount = depositAmount;
+        double profit = 0;
+
+        for (int i = 0; i < months; i++)
+        {
+            double monthProfit = totalAmount * monthInterestRate;
+            totalAmount += monthProfit;
+            profit += monthProfit;
+        }
+
+        Console.WriteLine("Сумма вклада с учетом процентов: " + totalAmount);
+        Console.WriteLine("Прибыль: " + profit);
+    }
+}
