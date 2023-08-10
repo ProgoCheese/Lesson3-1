a2b1614099351015c8b8598bf7033ca0c226d96a
Artemii100500 <Artemii100500@gmail.com>
2023-08-10 18:38:11 +0700

    E

---
+++ L5T8/Program.cs
@@ -0,0 +1,38 @@
+﻿/// <summary>
+/// Таблица умножения
+/// </summary>
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        for(int i = 0; i <= number; i++)
+        {
+            Console.Write(i.ToString().PadRight(3, ' '));
+
+            Console.Write("|");
+        }
+
+        Console.WriteLine();
+
+        for(int i = 1; i <= number; i++)
+        {
+            Console.Write(i.ToString().PadRight(3, ' '));
+
+            Console.Write("|");
+
+            for (int j = 1; j <= number; j++)
+            {
+                int x = j * i;
+
+                Console.Write(x.ToString().PadRight(3,' '));
+
+                Console.Write("|");
+            }
+            Console.WriteLine();
+        }
+    }
+}
