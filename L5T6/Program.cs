9ff413e9b7b80e796574fbbd3c1c424d102655b6
Artemii100500 <Artemii100500@gmail.com>
2023-08-10 13:46:10 +0700

    c

--- L5T6/Program.cs
+++ L5T6/Program.cs
@@ -1,4 +1,30 @@
 ﻿class Program
 {
+    static void Main()
+    {
+        Console.WriteLine("Введите число");
+        double n = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Простые числа:");
+
+        for (int i = 2; i <= n; i++)
+        {
+            bool isSimpleNumder = true;
+
+            for(int j = 2; j < i; j++)
+            {
+                double x =(double) i % j;
+                if (x == 0)
+                {
+                    isSimpleNumder = false;
+                    break;
+                }
+            }
+
+            if (isSimpleNumder)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
 
 }
