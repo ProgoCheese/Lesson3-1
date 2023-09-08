a9983a0a9f6d6e243cca6da15878ce0080128df2
Artemii100500 <Artemii100500@gmail.com>
2023-09-08 20:54:36 +0700

    s

--- L5T8/Program.cs
+++ L5T8/Program.cs
@@ -9,7 +9,7 @@ class Program
         Console.WriteLine("Введите число");
         int number = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("   |");
+        Console.Write("   | ");
 
         for(int i = 1; i <= number; i++)
         {
@@ -20,7 +20,7 @@ class Program
 
         Console.Write("---+");
 
-        for (int i = 0; i <= number; i++)
+        for (int i = 0; i < number; i++)
         {
             Console.Write("----");
         }
@@ -29,9 +29,11 @@ class Program
 
         for(int i = 1; i <= number; i++)
         {
-            Console.Write(i.ToString().PadRight(3, ' '));
+            Console.Write(" ");
 
-            Console.Write("|");
+            Console.Write(i.ToString().PadRight(2, ' '));
+
+            Console.Write("| ");
 
             for (int j = 1; j <= number; j++)
             {
