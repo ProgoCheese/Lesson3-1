1b67d2e1dcf40b680a70189752ac878f1b70c4ee
Artemii100500 <Artemii100500@gmail.com>
2023-09-08 21:36:06 +0700

    q

--- L6T5/Program.cs
+++ L6T5/Program.cs
@@ -5,32 +5,35 @@ class Program
 {
     static void Main()
     {
-        int priceFirst = PrintAndRead("Введите стоймость 1 блюда:");
-        int priceSecondt = PrintAndRead("Введите стоймость 2 блюда:");
+        double priceFirst = PrintAndRead("Введите стоймость 1 блюда:");
+        double priceSecondt = PrintAndRead("Введите стоймость 2 блюда:");
 
-        int quantityFirst = PrintAndRead("Введите количество 1 блюда:");
-        int quantitySecond = PrintAndRead("Введите количество 2 блюда:");
+        double quantityFirst = PrintAndRead("Введите количество 1 блюда:");
+        double quantitySecond = PrintAndRead("Введите количество 2 блюда:");
 
         double priceAll = priceFirst * quantityFirst + priceSecondt * quantitySecond;
 
+        CalculationDiscount(quantityFirst, quantitySecond, priceAll);
+    }
+
+    public static void CalculationDiscount(double quantityFirst, double quantitySecond, double priceAll)
+    {
         double discount = 1;
 
-        if(quantityFirst + quantitySecond >= 10 && priceAll >= 1000)
+        if (quantityFirst + quantitySecond >= 10 && priceAll >= 1000)
         {
             discount = 0.9;
         }
-        else if(priceAll >= 1000)
+        else if (priceAll >= 1000)
         {
             discount = 0.95;
         }
-        else if(quantityFirst + quantitySecond >= 10)
+        else if (quantityFirst + quantitySecond >= 10)
         {
             discount = 0.95;
         }
 
-        priceAll = priceAll * discount;
-
-        Console.WriteLine("Стоймость заказа составила {0}",priceAll);
+        Console.WriteLine("Стоймость заказа составила {0}", priceAll* discount);
     }
 
     public static int PrintAndRead(string prompt)
