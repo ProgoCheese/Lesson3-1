9434a35adfdf3423a3c9d8be71466bb0562b1219
Artemii <artemii100500@gmail.com>
2024-07-05 15:43:11 +0700

    L7T2

---
+++ L7T2/Program.cs
@@ -0,0 +1,36 @@
+﻿/// <summary>
+/// Range
+/// </summary>
+class Program
+{
+    public class Range
+    {
+        public int From { get; set; } 
+        public int To { get; set; }
+
+        //На самом деле если мы принимаем 2 вещественных числа то количество будет бесконечно т.к. вещественных бесконечно
+        // Так что будем считать что 2 целочисленных числа и что from < to, иначе задача не имеет решения
+        public int GetLength()
+        {
+            return To - From + 1;
+        }
+
+        public bool IsInside(int number)
+        {
+            if(number >  From && number < To) return true;
+            else return false;
+        }
+    }
+
+    static void Main()
+    {
+        Range userRange = new Range { };
+        Console.WriteLine("Введите первое число: ");
+        userRange.From = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите последнее число: ");
+        userRange.To = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Длина: {userRange.GetLength()}");
+        Console.WriteLine("Введите число: ");
+        Console.WriteLine($"Число находится в последовательности? {userRange.IsInside(Convert.ToInt32(Console.ReadLine()))}");
+    }
+}
