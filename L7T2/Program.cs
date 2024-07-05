30dc2d2b43e32327000b926d1cd82029c3162dfb
Artemii <artemii100500@gmail.com>
2024-07-05 22:28:49 +0700

    1

--- L7T2/Program.cs
+++ L7T2/Program.cs
@@ -5,31 +5,52 @@ class Program
 {
     public class Range
     {
-        public int From { get; set; } 
-        public int To { get; set; }
+        public double from;
+        public double to;
 
-        //На самом деле если мы принимаем 2 вещественных числа то количество будет бесконечно т.к. вещественных бесконечно
-        // Так что будем считать что 2 целочисленных числа и что from < to, иначе задача не имеет решения
-        public int GetLength()
+        public Range(double userFrom, double userTo)
         {
-            return To - From + 1;
+            this.from = userFrom;
+            this.to = userTo;
         }
 
-        public bool IsInside(int number)
+        //На самом деле если мы принимаем 2 вещественных числа то количество будет бесконечно т.к. вещественных бесконечно
+        //Так что будем считать что 2 целочисленных числа и что from < to, иначе задача не имеет решения
+        //Проблема с вещественными такая что между 2 и 2.5 сколько чисел? Или 0 или бесконечность. 
+        //Вот был бы ты на лекции Гутмана про самую знаменитую задачу 20 века про гипотизу континиума то понял
+        //Можешь почитать https://habr.com/ru/articles/766446/ я не знаю как там все точно расписано но суть такая что
+        //вещественные числа это континиум, даже если между 2 любыми. Так что вот
+        //Или просто объясни в чем прикол
+
+        public double GetLength()
         {
-            if(number >  From && number < To) return true;
-            else return false;
+            return this.to - this.from + 1;
+        }
+
+        public bool IsInside(double number)
+        {
+            if (number > this.from && number < this.to)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 
     static void Main()
     {
-        Range userRange = new Range { };
+
         Console.WriteLine("Введите первое число: ");
-        userRange.From = Convert.ToInt32(Console.ReadLine());
+        int firstNumber = Convert.ToInt32(Console.ReadLine());
+        
         Console.WriteLine("Введите последнее число: ");
-        userRange.To = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine($"Длина: {userRange.GetLength()}");
+        int secondNumber = Convert.ToInt32(Console.ReadLine());
+        
+        Range userRange = new Range(firstNumber,secondNumber);
+
+        Console.WriteLine("Длина: Бесконечность, давай сделай мне последовательность из 0.5 чисел");
+        
         Console.WriteLine("Введите число: ");
         Console.WriteLine($"Число находится в последовательности? {userRange.IsInside(Convert.ToInt32(Console.ReadLine()))}");
     }
