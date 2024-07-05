a24ddcfbc0e4b59b2298d1217f7c37f09cad3fda
Artemii <artemii100500@gmail.com>
2024-07-05 14:34:38 +0700

    L7T1

---
+++ L7T1/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+   public class Contact
+   {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public double Numders { get; set; }
+   }
+
+    static void Main()
+    {
+        Contact user = new Contact();
+        user.Name = "Andrei";
+        user.Surname = "Ivanov";
+        user.Numders = 89839245029;
+        Console.WriteLine($"Имя: {user.Name}, Фамилия: {user.Surname}, Номер: {user.Numders}.");
+        Contact friend = new Contact();
+        friend.Name = "Петя";
+        friend.Surname = "Петров";
+        friend.Numders = 9712397087;
+        Console.WriteLine($"Имя: {friend.Name}, Фамилия: {friend.Surname}, Номер: {friend.Numders}.");
+    }
+} 
