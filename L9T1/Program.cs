2478eaf9ab7238c819b06136ad90a5ce0623ac86
Artemii <artemii100500@gmail.com>
2024-07-06 15:51:59 +0700

    L9T1

---
+++ L9T1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lesson3_1
+{
+    /// <summary>
+    /// от 1 до 100
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = new int[100];
+
+            for (int i = 1; i <= arr.Length; i++)
+            {
+                arr[i - 1] = i;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
