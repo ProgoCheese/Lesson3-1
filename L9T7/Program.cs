f20d42ddfde60ed48edc7792b0979ec6ee7aad62
Artemii <artemii100500@gmail.com>
2024-07-06 17:56:30 +0700

    L9T7

---
+++ L9T7/Program.cs
@@ -0,0 +1,31 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Lesson3_1
+{
+    /// <summary>
+    /// max
+    /// </summary>
+    class Program
+    {
+        public static void GetUpperstring(string[] s)
+        {
+            for (int i = 0; i < s.Length; i++)
+            { 
+                s[i] = s[i].ToUpper();
+            }
+        }
+
+        static void Main()
+        {
+            string[] arr = { " dafiEeijd ", " ФУАщрф ", " FWEFFSEFsf ", " zsdfGFsd ", " SEsdAEDg ", "afdagas " };
+
+            GetUpperstring(arr);
+
+            foreach (string e in arr)
+            {
+                Console.Write(e);
+            }
+
+        }
+    }
+}
