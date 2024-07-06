aa3411d9f19c0ed45d065139180ec4676b40c3e6
Artemii <artemii100500@gmail.com>
2024-07-06 16:11:13 +0700

    L9T3

---
+++ L9T3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Lesson3_1
+{
+    /// <summary>
+    /// параметры
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length);
+
+            foreach (string e in args)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
