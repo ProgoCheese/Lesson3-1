571fbf6d5cd7861aa91b58c9a13ae71ab5c10d3d
Artemii <artemii100500@gmail.com>
2024-07-06 12:59:58 +0700

    L8T2

---
+++ L8T2/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Lesson3_1
+{
+    /// <summary>
+    /// Макс. строка
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите строку: ");
+            string userString = Console.ReadLine();
+
+            int maxString = 0;
+            int currentMax = 1;
+
+            for (int i = 1; i < userString.Length; i++)
+            {
+                char currentChar = userString[i];
+                char previousChar = userString[i - 1];
+
+                if (previousChar == currentChar)
+                {
+                    currentMax++;
+                }
+                else
+                {
+                    if (maxString < currentMax)
+                    {
+                        maxString = currentMax;
+                        currentMax = 0;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Максимальная подстрока {maxString}");
+        }
+    }
+}
