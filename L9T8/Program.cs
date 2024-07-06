204433af434dd11ece820cbf4753112a7c900b78
Artemii <artemii100500@gmail.com>
2024-07-06 21:12:26 +0700

    L9T8

---
+++ L9T8/Program.cs
@@ -0,0 +1,29 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Lesson3_1
+{
+    /// <summary>
+    /// AM
+    /// </summary>
+    class Program
+    {
+        static void Main()
+        {
+            int[] arr = {1,2,6,2,8,9,12,945,23};
+
+            int sum = 0;
+            int count = 0;
+
+            foreach (int e in arr)
+            {
+                if(e % 2 == 0)
+                {
+                    sum += e;
+                    count++;
+                }                
+            }
+
+            Console.WriteLine(sum / count);
+        }
+    }
+}
