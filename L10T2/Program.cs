66834f9cb0f4fec5c4553a912299e94bc07454da
Artemii <artemii100500@gmail.com>
2024-07-08 13:40:22 +0700

    L10T2

---
+++ L10T2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.SqlTypes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Lesson3_1
+{
+    /// <summary>
+    /// нод
+    /// </summary>
+    class Program
+    {
+        public static int GetNOD(int a, int b)
+        {
+            if(a % b == 0)
+            {
+                return b;
+            }
+            else
+            {
+                return GetNOD(b, a%b);
+            }
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Введите 2 числа");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(GetNOD(a, b));
+        }
+    }
+}
