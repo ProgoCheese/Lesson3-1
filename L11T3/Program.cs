bc7d3fe48791237542643fb4a4663497d3e36879
Artemii <artemii100500@gmail.com>
2024-07-08 20:19:36 +0700

    L11T3

---
+++ L11T3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.SqlTypes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Lesson3_1
+{
+    /// <summary>
+    /// нод
+    /// </summary>
+    class Program
+    {
+        public static void SortArr(int[] a)
+        {
+            for (int j = 0; j < a.Length - 1; j++)
+            {
+                for (int i = 0; i < a.Length - j - 1; i++)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        int temp = a[i + 1];
+                        a[i + 1] = a[i];
+                        a[i] = temp;
+
+
+                    }
+                }
+            }
+        }
+
+        static void Main()
+        {
+            int[] a = { 2, 5, 8, 1, 32, 975, 23, 0, 42, 66 };
+
+            SortArr(a);
+
+            Console.WriteLine();
+
+            foreach (int i in a)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
