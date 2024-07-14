d728c4b6bdfa48d883a546c689426ec1bda451d8
Artemii <artemii100500@gmail.com>
2024-07-14 23:07:50 +0700

    zfsd

--- L11T5/Program.cs
+++ L11T5/Program.cs
@@ -1,38 +1,46 @@
-﻿using System;
-using System.Data.SqlTypes;
-using static System.Runtime.InteropServices.JavaScript.JSType;
-
-namespace Lesson3_1
+﻿namespace Lesson3_1
 {
-    /// <summary>
-    /// нод
-    /// </summary>
     class Program
     {
-        public static void Swap(ref int a, ref int b)
-        {
-            a = b + a;
-            b = a - b;
-            a = a - b;
-        }
-
-        //public static int[] QuickSort(int[] a, int left, int right)
-        //{
-
-        //}
-
         static void Main()
         {
-            int[] a = { 2, 5, 8, 1, 32, 975, 23, 0, 42, 66 };
+            int[] array = [2, 5, 8, 1, 32, 975, 23, 0, 42, 66];
 
-            //QuickSort(a, 0, a.Length - 1);
+            QuickSort(array, 0, array.Length - 1);
 
-            Console.WriteLine();
-
-            foreach (int i in a)
+            foreach (int item in array)
             {
-                Console.Write(i + " ");
+                Console.Write(item + " ");
             }
+            Console.WriteLine();
+        }
+
+        static void QuickSort(int[] array, int low, int high)
+        {
+            if (low < high)
+            {
+                int pivotIndex = Partition(array, low, high);
+                QuickSort(array, low, pivotIndex - 1);
+                QuickSort(array, pivotIndex + 1, high);
+            }
+        }
+
+        static int Partition(int[] array, int low, int high)
+        {
+            int pivot = array[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (array[j] <= pivot)
+                {
+                    i++;
+                    (array[i], array[j]) = (array[j], array[i]);
+                }
+            }
+
+            (array[i + 1], array[high]) = (array[high], array[i + 1]);
+            return i + 1;
         }
     }
 }
