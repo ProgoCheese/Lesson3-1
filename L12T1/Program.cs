4ae53b1f3a4357beb06c450413bb01677d0fc3cc
Artemii <artemii100500@gmail.com>
2024-07-18 13:33:08 +0700

    a

---
+++ L12T1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace Lesson3_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            StringBuilder mySting = new StringBuilder();
+
+            mySting.Append(1);
+
+            for (int i = 1; i <= 100; i++)
+            {
+                mySting.Append(",")
+                    .Append(i);
+            }
+
+            Console.WriteLine(mySting.ToString());
+        }
+    }
+}
