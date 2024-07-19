12462766dfa71f530d718e69e83645a12ca93734
Artemii <artemii100500@gmail.com>
2024-07-19 23:42:13 +0700

    s

---
+++ L12T5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace Lesson3_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] lines = new string[100];
+
+            string file;
+
+            int countLine = 0;
+
+            using (StreamReader reader = new StreamReader("input.txt"))
+            {
+                while((file= reader.ReadLine()!) != null)
+                {
+                    if (countLine < lines.Length - 1)
+                    {
+                        lines[countLine] = file;
+                        countLine++;
+                    }
+                }
+            }
+
+
+            for(int i = 0; i < countLine; i++) 
+            {
+                Console.WriteLine(lines[i]);
+            }
+        }
+    }
+}
