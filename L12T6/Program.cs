8f372e84cc4299bb4861c6f2d129d8b9142fe2aa
Artemii <artemii100500@gmail.com>
2024-07-20 00:00:12 +0700

    ы

--- L12T6/Program.cs
+++ L12T6/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+namespace Lesson3_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] lines = new string[100];
+
+            string file;
+
+            int countLine = 0;
+
+            using (StreamReader reader = new StreamReader("input.txt"))
+            {
+                while ((file = reader.ReadLine()!) != null)
+                {
+                    if (countLine < lines.Length - 1)
+                    {
+                        lines[countLine] = file.ToUpper();
+                        countLine++;
+                    }
+                }
+
+                using (StreamWriter writer = new StreamWriter("output.txt"))
+                {
+                    for (int i = 0; i < countLine; i++)
+                    {
+                        writer.WriteLine(lines[i]);
+                    }
+                }
+            }
+        }
+    }
+}
