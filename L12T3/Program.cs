7aedafbbc4f1804e771e269a6f112a7ed6a668de
Artemii <artemii100500@gmail.com>
2024-07-19 14:47:44 +0700

    s

---
+++ L12T3/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+
+namespace Lesson3_1
+{
+    class Program
+    {
+        static int CountOccurrences(string text, string searchString)
+        {
+            if (string.IsNullOrEmpty(text) || string.IsNullOrEmpty(searchString))
+            {
+                return 0;
+            }
+
+            text = text.ToLower();
+            searchString = searchString.ToLower();
+
+            int count = 0;
+            int index = 0;
+
+            while ((index = text.IndexOf(searchString, index)) != -1)
+            {
+                count++;
+                index += searchString.Length;
+            }
+
+            return count;
+        }
+
+
+        /// <summary>
+        /// я чет за тупил и сделал не по заданию зато это работает и это быстрее
+        /// </summary>
+        //public static int CountString(string file, string line)
+        //{
+        //    int count = 0;
+
+        //    string[] word = file.Split(line, StringSplitOptions.RemoveEmptyEntries);
+
+        //    if (file.EndsWith(line) && file.StartsWith(line))
+        //    {
+        //        count = word.Length - 1 + 2;
+        //    }
+        //    else if (file.EndsWith(line) || file.StartsWith(line))
+        //    {
+        //        count = word.Length - 1 + 1;
+        //    }
+        //    else
+        //    {
+        //        count = word.Length - 1;
+        //    }
+
+        //    return count;
+        //}
+
+        static void Main()
+        {
+            Console.WriteLine("Введите строку");
+            string line = Console.ReadLine()!;
+
+            int count;
+
+            using (StreamReader reader = new StreamReader("input.txt"))
+            {
+                string file = reader.ReadLine()!;
+
+                count = CountOccurrences(file, line);
+            }
+
+            Console.WriteLine("В файл входит " + count + " раз");
+        }
+    }
+}
