ce3df9186dff128fe8e5b60ba0e0b83f6a8a0e2c
Artemii <artemii100500@gmail.com>
2024-07-30 01:28:04 +0700

    s

--- L12T6/Program.cs
+++ L12T6/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Text;
-
-namespace Lesson3_1
+﻿namespace Lesson3_1
 {
     class Program
     {
@@ -12,23 +10,14 @@ namespace Lesson3_1
 
             int countLine = 0;
 
-            using (StreamReader reader = new StreamReader("input.txt"))
-            {
-                while ((file = reader.ReadLine()!) != null)
-                {
-                    if (countLine < lines.Length - 1)
-                    {
-                        lines[countLine] = file.ToUpper();
-                        countLine++;
-                    }
-                }
+            using StreamReader reader = new("input.txt");
+            using StreamWriter writer = new("output.txt");
 
-                using (StreamWriter writer = new StreamWriter("output.txt"))
+            while ((file = reader.ReadLine()!) != null)
+            {
+                if (countLine < lines.Length - 1)
                 {
-                    for (int i = 0; i < countLine; i++)
-                    {
-                        writer.WriteLine(lines[i]);
-                    }
+                    writer.WriteLine(file.ToUpper());
                 }
             }
         }
