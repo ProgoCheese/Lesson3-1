851063c9903dafe33139ef087f9cdad33d370df4
Artemii <artemii100500@gmail.com>
2024-07-29 23:37:26 +0700

    ф

--- L12T4/Program.cs
+++ L12T4/Program.cs
@@ -8,16 +8,14 @@ namespace Lesson3_1
         {
             Console.WriteLine("Введите строку");
             string line = "1,2,3,4,5";
-            string[] numbers = line .Split(",", StringSplitOptions.RemoveEmptyEntries);
+            string[] numbers = line.Split(",", StringSplitOptions.RemoveEmptyEntries);
 
             int sum = 0;
 
             foreach (string number in numbers)
             {
-                sum += Convert.ToInt32(number);                
+                sum += Convert.ToInt32(number);
             }
-
-            
 
             Console.WriteLine("Сумма " + sum);
         }
